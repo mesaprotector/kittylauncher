1b8213a1234c30cffb05f9b06a6af49ef6b2c790
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-09-05 02:01:12 -0700

    Shorten $HOME path

--- kittylauncher.sh
+++ kittylauncher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "/home/asuka/.local/state/kittylauncher";
+cd "~/.local/state/kittylauncher";
 PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' kitty -T "Kitty Launcher" sh -c 'bash -t';
-cd "/home/asuka/.local/state/kittylauncher";
+cd "~/.local/state/kittylauncher";
 nohup ~/.local/state/kittylauncher/_output
