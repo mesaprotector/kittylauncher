d314befa99062ce0517c7db0998cda8513fda7e0
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-10-16 17:17:06 -0700

    Run from home dir while still sending output to kittylauncher dir

--- kittylauncher.sh
+++ kittylauncher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "~/.local/state/kittylauncher";
 PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' kitty -T "Kitty Launcher" sh -c 'bash -t';
-cd "~/.local/state/kittylauncher";
-nohup ~/.local/state/kittylauncher/_output
+cd "~";
+nohup ~/.local/state/kittylauncher/_output > ~/.local/state/kittylauncher/nohup.out
