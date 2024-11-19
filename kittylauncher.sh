28a8e7dd800629239a8e8df7edcd6bfd983b5690
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-11-18 20:33:34 -0800

    Turn caps lock off if it is on when running script
    Relevant because xdotool, which is used to pre-fill "kitty-launch", will type it in caps if caps lock is on. Once this switches to a display-server-agnostic method this can be removed.

--- kittylauncher.sh
+++ kittylauncher.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd "~/.local/state/kittylauncher";
+if [ `xset q | grep Caps | awk '{print $4}'` = "on" ]; then
+	xdotool key Caps_Lock
+fi
 PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' kitty -T "Kitty Launcher" sh -c 'bash -t';
 cd "~";
 nohup ~/.local/state/kittylauncher/_output > ~/.local/state/kittylauncher/nohup.out
