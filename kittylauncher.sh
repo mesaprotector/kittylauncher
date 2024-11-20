963b95091ba316ea8da5147f78b1357a0677273f
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-11-19 18:43:48 -0800

    Add fullly configurable paths
    This was a 5-line script just yesterday...

--- kittylauncher.sh
+++ kittylauncher.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
-cd "~/.local/state/kittylauncher";
+# Define all directories first. Probably easier to do this another way.
+if [ -z "$XDG_CONFIG_HOME" ]; then 
+	configdir="~/.config"
+else
+	configdir="$XDG_CONFIG_HOME"
+fi
+statedir=`cat $configdir/kittylauncherrc | grep ^statedir | cut -d '=' -f 2-2`
+if [ -z "$statedir" ]; then
+	if [ -z "$XDG_STATE_HOME" ]; then
+		statedir="~/.local/state/kittylauncher"
+	else
+		statedir="$XDG_STATE_HOME/kittylauncher"
+	fi
+fi
+workdir=`cat $configdir/kittylauncherrc | grep ^workdir | cut -d '=' -f 2-2`
+if [ -z "$workdir" ]; then
+	workdir="$HOME"
+fi
+configdir=`eval echo $configdir`
+statedir=`eval echo $statedir`
+workdir=`eval echo $workdir`
+cd $statedir;
+# Check if caps lock is on and turn it off if so.
 if [ `xset q | grep Caps | awk '{print $4}'` = "on" ]; then
 	xdotool key Caps_Lock
 fi
+# Launch terminal.
 PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' kitty -T "Kitty Launcher" sh -c 'bash -t';
-cd "~";
-nohup ~/.local/state/kittylauncher/_output > ~/.local/state/kittylauncher/nohup.out
+# Run command from workdir.
+cd $workdir;
+nohup $statedir/_output > $statedir/nohup.out
