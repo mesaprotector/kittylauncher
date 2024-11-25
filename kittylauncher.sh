c1fe3402169112dc3b12b752460d87fb5f5f7fff
mesaprotector <162765448+mesaprotector@users.noreply.github.com>
2024-11-25 01:14:42 -0800

    Better formatting and quoting

--- kittylauncher.sh
+++ kittylauncher.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 # Define all directories first. Probably easier to do this another way.
 if [ -z "$XDG_CONFIG_HOME" ]; then 
-	configdir="~/.config"
+	configdir="$HOME/.config"
 else
 	configdir="$XDG_CONFIG_HOME"
 fi
-statedir=`cat $configdir/kittylauncherrc | grep ^statedir | cut -d '=' -f 2-2`
+statedir="`cat "$configdir"/kittylauncherrc | grep ^statedir \
+| cut -d '=' -f 2-2`"
 if [ -z "$statedir" ]; then
 	if [ -z "$XDG_STATE_HOME" ]; then
-		statedir="~/.local/state/kittylauncher"
+		statedir="$HOME/.local/state/kittylauncher"
 	else
 		statedir="$XDG_STATE_HOME/kittylauncher"
 	fi
 fi
-workdir=`cat $configdir/kittylauncherrc | grep ^workdir | cut -d '=' -f 2-2`
+workdir="`cat "$configdir"/kittylauncherrc | grep ^workdir | cut -d '=' -f 2-2`"
 if [ -z "$workdir" ]; then
 	workdir="$HOME"
 fi
-configdir=`eval echo $configdir`
-statedir=`eval echo $statedir`
-workdir=`eval echo $workdir`
-cd $statedir;
+configdir="`eval echo "$configdir"`"
+statedir="`eval echo "$statedir"`"
+workdir="`eval echo "$workdir"`"
+cd "$statedir" || exit;
 # Check if caps lock is on and turn it off if so.
-if [ `xset q | grep Caps | awk '{print $4}'` = "on" ]; then
+if [ "`xset q | grep Caps | awk '{print $4}'`" = "on" ]; then
 	xdotool key Caps_Lock
 fi
 # Launch terminal.
-PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' kitty -T "Kitty Launcher" sh -c 'bash -t';
+PROMPT_COMMAND='PS1="! > "; xdotool type "kitty-launch "' \
+kitty -T "Kitty Launcher" sh -c 'bash -t';
 # Run command from workdir.
-cd $workdir;
-nohup $statedir/_output > $statedir/nohup.out
+cd "$workdir" || exit;
+nohup "$statedir"/_output > "$statedir"/nohup.out
